4ab233d8884f5a715fd0ed4545a2ed3ec6622ccb
N4yth <nathan.dupuis31@gmail.com>
2026-03-09 16:49:52 +0100

    dox : put somme documentation and execute a good time

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -5,6 +5,7 @@ VALID_CODES = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
 
 def print_stats(total_size, status_counts):
+	"""print all the stats"""
 	print("File size: {}".format(total_size))
 	for code in sorted(status_counts.keys()):
 		if status_counts[code] > 0:
@@ -38,3 +39,6 @@ def main():
 	except KeyboardInterrupt:
 		pass
 	print_stats(total_size, status_counts)
+
+if __name__ == "__main__":
+    main()
