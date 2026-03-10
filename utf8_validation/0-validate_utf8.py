08e8bb1c866147c8f3345b65a2ee5f851a854ea1
N4yth <nathan.dupuis31@gmail.com>
2026-03-10 11:03:54 +0100

    feat: refactor validUTF8 function to improve UTF-8 validation logic

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -7,9 +7,25 @@ def validUTF8(data):
     """
     test if data is utf-8 encodage
     """
-    idx = 0
-    while (len(data) > idx):
-        if (data[idx] > 255):
-            return False
-        idx+=1
-    return True
+    remaining = 0
+
+    for num in data:
+        byte = num & 0xFF
+        if remaining == 0:
+            mask = 0b10000000
+            count = 0
+            while mask & byte:
+                count += 1
+                mask >>= 1
+            if count == 0:
+                continue
+
+            if count == 1 or count > 4:
+                return False
+
+            remaining = count - 1
+        else:
+            if (byte & 0b11000000) != 0b10000000:
+                return False
+            remaining -= 1
+    return remaining == 0
