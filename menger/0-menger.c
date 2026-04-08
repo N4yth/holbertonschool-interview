797cf5df8d76ec2b4d4b81402fc6145bfa277490
N4yth <nathan.dupuis31@gmail.com>
2026-04-08 16:14:55 +0200

    feat: implement menger function and header file for Menger sponge generation

---
+++ menger/0-menger.c
@@ -0,0 +1,38 @@
+#include "menger.h"
+
+void menger(int level)
+{
+    int x;
+    int y;
+    int size = (int)pow(3, level);
+    int hole = 0;
+
+    if (level >= 0)
+    {
+        for (x = 0; x < size; x++)
+        {
+            for (y = 0; y < size ; y++)
+            {
+                hole = 0;
+                int tx = x;
+                int ty = y;
+
+                while (tx > 0 || ty > 0)
+                {
+                    if (tx % 3 == 1 && ty % 3 == 1)
+                    {
+                        hole = 1;
+                        break;
+                    }
+                    tx /= 3;
+                    ty /= 3;
+                }
+                if (hole)
+                    printf (" ");
+                else
+                    printf ("#");
+            }
+            printf("\n");
+        }
+    }
+}
---
+++ menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif
