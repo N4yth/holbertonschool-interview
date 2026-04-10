e568ed94f2d0b8475f93dcdde6dd4245d3abd570
N4yth <nathan.dupuis31@gmail.com>
2026-04-10 14:39:50 +0200

    style: add parentheses for consistency in sorted_array_to_avl function return statement

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -63,5 +63,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	if (!array || size == 0)
 		return (NULL);
 
-	return build_avl(array, 0, size - 1);
+	return (build_avl(array, 0, size - 1));
 }
