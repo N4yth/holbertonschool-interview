357f1bb431a7f70e6a64d2fbb8801a1ad1db7c50
N4yth <nathan.dupuis31@gmail.com>
2026-04-10 14:39:07 +0200

    style: improve code formatting for consistency and readability in sorted_array_to_avl.c

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,34 +1,45 @@
 #include "binary_trees.h"
 
-
+/**
+ * create_node - create a node
+ *
+ * @value: Value to put
+ */
 avl_t *create_node(int value)
 {
 	avl_t *node = malloc(sizeof(avl_t));
+
 	if (!node)
-		return(NULL);
+		return (NULL);
 
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
 	node->parent = NULL;
 
-	return(node);
+	return (node);
 }
 
-
+/**
+ * build_avl - build the AVL
+ *
+ * @array: The array to be transform
+ * @start: start of the array to AVL
+ * @end: end of the array to AVL
+ */
 static avl_t *build_avl(int *array, int start, int end)
 {
 	int mid;
 	avl_t *root;
 
 	if (start > end)
-		return(NULL);
+		return (NULL);
 
 	mid = (start + end) / 2;
 
 	root = create_node(array[mid]);
 	if (!root)
-		return(NULL);
+		return (NULL);
 
 	root->left = build_avl(array, start, mid - 1);
 	if (root->left)
@@ -38,14 +49,19 @@ static avl_t *build_avl(int *array, int start, int end)
 	if (root->right)
 		root->right->parent = root;
 
-	return(root);
+	return (root);
 }
 
-
+/**
+ * sorted_array_to_avl - Sorted an array into an AVL
+ *
+ * @array: The array to be Sorted
+ * @size: Size of the array
+ */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (!array || size == 0)
-		return(NULL);
+		return (NULL);
 
 	return build_avl(array, 0, size - 1);
 }
