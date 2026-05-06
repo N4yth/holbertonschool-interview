50507ff900a206a253f57c229f1ed238683ea031
N4yth <nathan.dupuis31@gmail.com>
2026-05-06 11:21:06 +0200

    style: refactor starwars character fetching to use async/await for improved readability

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2] + '/'
-const request = new Request(url);
-var requestcharacter;
 
-fetch(request).then((response) => {
-  return response.json();
-})
-  .then((datafilm) => {
-    datafilm.characters.forEach(Characters => {
-        requestcharacter = new Request(Characters);
-        fetch(requestcharacter).then((response) => {
-            return response.json();
-            })
-            .then((datacharacters) => {
-                console.log(datacharacters.name)
-            });
-    });
-});
+async function main() {
+  const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}/`;
+
+  const response = await fetch(url);
+  const datafilm = await response.json();
+
+  for (const characterUrl of datafilm.characters) {
+    const res = await fetch(characterUrl);
+    const data = await res.json();
+    console.log(data.name);
+  }
+}
+
+main();
