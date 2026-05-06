414f26a9de54047b60807c933b59bd5cb2d6f28c
N4yth <nathan.dupuis31@gmail.com>
2026-05-06 15:46:57 +0200

    style: refactor rainwater calculation for improved efficiency and clarity

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -6,17 +6,27 @@ code that return Integer indicating total amount of rainwater retained.
 
 def rain(walls):
     """return Integer indicating total amount of rainwater retained."""
-    result = 0
-    tmp = 0
-    if not walls:
+    if not walls or len(walls) < 3:
         return 0
-    for i in range(len(walls)):
-        if walls[i] > 0 and walls[tmp] > 0 and i > 0:
-            if walls[i] > walls[tmp]:
-                result += walls[tmp] * (i - tmp - 1)
+
+    left = 0
+    right = len(walls) - 1
+    left_max = 0
+    right_max = 0
+    water = 0
+
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
             else:
-                result += walls[i] * (i - tmp - 1)
-        if walls[i] > 0:
-            tmp = i
-            
-    return result
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+
+    return water
