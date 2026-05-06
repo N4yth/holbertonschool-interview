d3a6f22cd7d66c614c336e6620c6901d96642c55
N4yth <nathan.dupuis31@gmail.com>
2026-05-06 10:10:42 +0200

    style: improve code formatting and remove unnecessary whitespace in linear_skip function and search.h

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -7,20 +7,15 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	if (!list)
 		return (NULL);
-
 	while (list->express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 			list->express->index, list->express->n);
-
 		if (list->express->n >= value)
 			break;
-
 		list = list->express;
 	}
-
 	prev = list;
-
 	if (list->express)
 	{
 		printf("Value found between indexes [%lu] and [%lu]\n",
@@ -33,23 +28,17 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 		while (tail->next)
 			tail = tail->next;
-
 		printf("Value found between indexes [%lu] and [%lu]\n",
 			list->index, tail->index);
-
 		list = tail;
 	}
-
 	while (prev && prev->index <= list->index)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 			prev->index, prev->n);
-
 		if (prev->n == value)
 			return (prev);
-
 		prev = prev->next;
 	}
-
 	return (NULL);
 }
