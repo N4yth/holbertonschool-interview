8b8d2fb77a97a3eaba20ca7407ccc0ee432bf8b2
N4yth <nathan.dupuis31@gmail.com>
2026-05-06 15:37:07 +0200

    style: add initial implementation of rainwater retention calculation and main execution script

---
+++ rain/0-rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+code that return Integer indicating total amount of rainwater retained.
+"""
+
+
+def rain(walls):
+    """return Integer indicating total amount of rainwater retained."""
+    result = 0
+    tmp = 0
+    if not walls:
+        return 0
+    for i in range(len(walls)):
+        if walls[i] > 0 and walls[tmp] > 0 and i > 0:
+            if walls[i] > walls[tmp]:
+                print(walls[tmp] * (i - tmp - 1))
+                result += walls[tmp] * (i - tmp - 1)
+            else:
+                print(walls[i] * (i - tmp - 1))
+                result += walls[i] * (i - tmp - 1)
+        if walls[i] > 0:
+            tmp = i
+            
+    return result
