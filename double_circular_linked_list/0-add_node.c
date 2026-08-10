ca64331b32e7d77f0084bb292c8d521316f504c2
N4yth <nathan.dupuis31@gmail.com>
2026-08-10 13:15:14 +0200

    style: standardize return statements in add_node functions

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -15,17 +15,17 @@ List *add_node_begin(List **list, char *str)
 	List *new_node, *tmp;
 
 	if (list == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	if (*list == NULL)
@@ -33,7 +33,7 @@ List *add_node_begin(List **list, char *str)
 		new_node->next = new_node;
 		new_node->prev = new_node;
 		*list = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	tmp = (*list)->prev;
@@ -43,7 +43,7 @@ List *add_node_begin(List **list, char *str)
 	(*list)->prev = new_node;
 	*list = new_node;
 
-	return new_node;
+	return (new_node);
 }
 
 /**
@@ -58,17 +58,17 @@ List *add_node_end(List **list, char *str)
 	List *new_node, *tmp;
 
 	if (list == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	if (*list == NULL)
@@ -76,7 +76,7 @@ List *add_node_end(List **list, char *str)
 		new_node->next = new_node;
 		new_node->prev = new_node;
 		*list = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	tmp = (*list)->prev;
@@ -85,5 +85,5 @@ List *add_node_end(List **list, char *str)
 	tmp->next = new_node;
 	(*list)->prev = new_node;
 
-	return new_node;
+	return (new_node);
 }
