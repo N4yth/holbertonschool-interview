01e0608c0bce50117e42135ef9911530bfd0c69d
N4yth <nathan.dupuis31@gmail.com>
2026-08-10 13:06:25 +0200

    style: improve comments and error handling in add_node functions

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -1,59 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
 /**
-* add_node_begin - add a node at the begin of the list
-*
+* add_node_begin - add a node at the beginning of the list
 * @list: the list to modify
 * @str: the string to add
 *
 * Return: NULL or the new node
 */
-List *add_node_begin(List **list, char *str);
+List *add_node_begin(List **list, char *str)
+{
 	List *new_node, *tmp;
 
-
-	if (*list == NULL)
+	if (list == NULL || *list == NULL)
 		return NULL;
 
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return NULL;
 
-	tmp = (*list)->prev
+	tmp = (*list)->prev;
 	new_node->next = *list;
 	new_node->prev = tmp;
 	tmp->next = new_node;
 	(*list)->prev = new_node;
 	new_node->str = str;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return NULL;
+	}
+
 	*list = new_node;
 
 	return new_node;
+}
 
 /**
 * add_node_end - add a node at the end of the list
-*
 * @list: the list to modify
 * @str: the string to add
 *
 * Return: NULL or the new node
 */
-List *add_node_end(List **list, char *str);
+List *add_node_end(List **list, char *str)
+{
 	List *new_node, *tmp;
 
-	if (*list == NULL)
+	if (list == NULL || *list == NULL)
 		return NULL;
 
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return NULL;
 
-	tmp = (*list)->prev
-	new_node->next = (*list);
+	tmp = (*list)->prev;
+	new_node->next = *list;
 	new_node->prev = tmp;
-	new_node->str = str;
 	tmp->next = new_node;
 	(*list)->prev = new_node;
+	new_node->str = str;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return NULL;
+	}
 
 	return new_node;
+}
