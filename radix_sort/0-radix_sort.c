05b245d013abe8634003fcda7c2b0d821cb9506a
N4yth <nathan.dupuis31@gmail.com>
2026-08-12 09:32:52 +0200

    style: improve formatting and consistency in radix_sort function documentation

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -2,13 +2,13 @@
 #include "sort.h"
 
 /**
- * radix_sort - sorts an array using LSD radix sort
- *
- * @array: array to sort
- * @size: size of the array
- *
- * Return : Nothing
- */
+* radix_sort - sorts an array using LSD radix sort
+*
+* @array: array to sort
+* @size: size of the array
+*
+* Return : Nothing
+*/
 void radix_sort(int *array, size_t size)
 {
 	int *output;
@@ -24,7 +24,8 @@ void radix_sort(int *array, size_t size)
 	if (output == NULL)
 		return;
 	max = array[0];
-	for (i = 1; i < (int)size; i++) {
+	for (i = 1; i < (int)size; i++) 
+	{
 		if (array[i] > max)
 			max = array[i];
 	}
@@ -32,17 +33,19 @@ void radix_sort(int *array, size_t size)
 	{
 		for (i = 0; i < 10; i++)
 			count[i] = 0;
-		for (i = 0; i < (int)size; i++) {
+		for (i = 0; i < (int)size; i++) 
+		{
 			digit = (array[i] / exp) % 10;
 			count[digit]++;
 		}
 		for (i = 1; i < 10; i++)
 			count[i] += count[i - 1];
-		for (i = (int)size - 1; i >= 0; i--) {
+		for (i = (int)size - 1; i >= 0; i--)
+		{
 			digit = (array[i] / exp) % 10;
 			output[count[digit] - 1] = array[i];
 			count[digit]--;
-        }
+		}
 		for (i = 0; i < (int)size; i++)
 			array[i] = output[i];
 		print_array(array, size);
