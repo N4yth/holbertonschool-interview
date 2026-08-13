c6205ae4d1b19516cf1e7131cca8fde3e7e1fcf1
N4yth <nathan.dupuis31@gmail.com>
2026-08-13 10:24:56 +0200

    feat: add wildcmp function and main execution for string comparison

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Compares two strings
+ *
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 1 if the strings can be identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		while (*s2 == '*')
+			s2++;
+		if (*s2 == '\0')
+			return (1);
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1, s2))
+				return (1);
+			s1++;
+		}
+		return (wildcmp(s1, s2));
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 1));
+}
