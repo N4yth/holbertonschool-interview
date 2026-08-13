f3a9af529bc2d3789fe2de49d59eea9a10d9af86
N4yth <nathan.dupuis31@gmail.com>
2026-08-13 10:36:40 +0200

    fix: optimize wildcmp function by simplifying star handling logic

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -12,17 +12,13 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*')
 	{
-		while (*s2 == '*')
-			s2++;
-		if (*s2 == '\0')
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1))
 			return (1);
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2))
-				return (1);
-			s1++;
-		}
-		return (wildcmp(s1, s2));
+		if (*s1 != '\0')
+			return (wildcmp(s1 + 1, s2));
+		return (0);
 	}
 
 	if (*s1 == '\0' && *s2 == '\0')
