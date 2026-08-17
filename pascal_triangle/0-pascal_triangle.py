07b8a364ba1bc50842806995bbd97283888ac60e
N4yth <nathan.dupuis31@gmail.com>
2026-08-17 10:56:46 +0200

    feat: add pascal_triangle function and README for Pascal's triangle implementation

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+module that returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
+
+def pascal_triangle(n):
+    """
+    function that returns a list of lists of integers
+    representing the Pascal's triangle of n
+    """
+    List = []
+    if n <= 0:
+        return List
+
+    past_row = []
+    row = []
+    len_list = 1
+    for y in range(0, n):
+        for i in range(0, len_list):
+            if i == 0 or i == len_list - 1:
+                row.append(1)
+            else:
+                row.append(past_row[i - 1] + past_row[i])
+        len_list += 1
+        List.append(row[:])
+        past_row = row[:]
+        row = []
+    return List
