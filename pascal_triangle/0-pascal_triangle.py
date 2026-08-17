17da3880f9a4f622427601b66f4c61eaeb32a5c4
N4yth <nathan.dupuis31@gmail.com>
2026-08-17 10:57:10 +0200

    style: add newline at end of file in pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,7 @@ module that returns a list of lists of integers
 representing the Pascal's triangle of n
 """
 
+
 def pascal_triangle(n):
     """
     function that returns a list of lists of integers
